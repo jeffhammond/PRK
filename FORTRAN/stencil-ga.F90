27ae26bc50d482e6daf82f1a00acde3f57b97c11
Jeff Hammond <jeff.r.hammond@intel.com>
2020-05-25 08:57:22 -0700

    GA stencil WIP

--- FORTRAN/stencil-ga.F90
+++ FORTRAN/stencil-ga.F90
@@ -1,20 +1,20 @@
 !
-! Copyright (c) 2013, Intel Corporation
+! Copyright (c) 2020, Intel Corporation
 !
 ! Redistribution and use in source and binary forms, with or without
 ! modification, are permitted provided that the following conditions
 ! are met:
 !
 ! * Redistributions of source code must retain the above copyright
-!       notice, this list of conditions and the following disclaimer.
+!      notice, this list of conditions and the following disclaimer.
 ! * Redistributions in binary form must reproduce the above
-!       copyright notice, this list of conditions and the following
-!       disclaimer in the documentation and/or other materials provided
-!       with the distribution.
+!      copyright notice, this list of conditions and the following
+!      disclaimer in the documentation and/or other materials provided
+!      with the distribution.
 ! * Neither the name of Intel Corporation nor the names of its
-!       contributors may be used to endorse or promote products
-!       derived from this software without specific prior written
-!       permission.
+!      contributors may be used to endorse or promote products
+!      derived from this software without specific prior written
+!      permission.
 !
 ! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 ! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -28,7 +28,10 @@
 ! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 ! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ! POSSIBILITY OF SUCH DAMAGE.
+
+!*******************************************************************
 !
+! NAME:    transpose
 !
 ! *******************************************************************
 !
@@ -57,7 +60,7 @@
 !            added constant to array "in" at end of each iteration to force
 !            refreshing of neighbor data in parallel versions; August 2013
 !          - Converted to Fortran by Jeff Hammond, January-February 2016.
-!
+!          - Global Arrays by Jeff Hammond, May 2020.
 ! *******************************************************************
 
 subroutine initialize_w(is_star,r,W)
@@ -174,25 +177,38 @@ end subroutine apply_stencil
 
 program main
   use iso_fortran_env
-#ifdef _OPENMP
-  use omp_lib
-#endif
+  use mpi_f08
   implicit none
-  real(kind=REAL64) :: prk_get_wtime
+#include 'global.fh'
+#include 'mafdecls.fh'
+!#include 'ga-mpi.fh' ! unused
   ! for argument parsing
   integer :: err
   integer :: arglen
   character(len=32) :: argtmp
+  ! MPI - should always use 32-bit INTEGER
+  integer(kind=INT32), parameter :: requested = MPI_THREAD_SERIALIZED
+  integer(kind=INT32) :: provided
+  integer(kind=INT32) :: world_size, world_rank
+  integer(kind=INT32) :: ierr
+  type(MPI_Comm), parameter :: world = MPI_COMM_WORLD
+  ! GA - compiled with 64-bit INTEGER
+  logical :: ok
+  integer :: me, np
+  integer :: A, B, AT
+  integer :: mylo(2),myhi(2)
+  real(kind=REAL64), parameter :: one  = 1.d0
+  real(kind=REAL64), allocatable ::  T(:,:)
   ! problem definition
-  integer(kind=INT32) :: iterations                    ! number of times to run the pipeline algorithm
-  integer(kind=INT32) :: n                             ! linear grid dimension
+  integer(kind=INT32) ::  iterations
+  integer(kind=INT32) :: n
   integer(kind=INT32) :: stencil_size                  ! number of points in stencil
   integer(kind=INT32) :: tile_size                     ! loop nest block factor
-  logical :: tiling                                    ! boolean indication loop nest blocking
+  integer(kind=INT64) :: bytes, max_mem
   logical :: is_star                                   ! true = star, false = grid
   integer(kind=INT32), parameter :: r=RADIUS           ! radius of stencil
   real(kind=REAL64) :: W(-r:r,-r:r)                    ! weights of points in the stencil
-  real(kind=REAL64), allocatable :: A(:,:), B(:,:)     ! grid values
+  real(kind=REAL64), allocatable :: TA(:,:), TB(:,:)   ! grid values
   real(kind=REAL64), parameter :: cx=1.d0, cy=1.d0
   ! runtime variables
   integer(kind=INT32) :: i, j, k
@@ -205,13 +221,6 @@ program main
   ! ********************************************************************
   ! read and test input parameters
   ! ********************************************************************
-
-  write(*,'(a25)') 'Parallel Research Kernels'
-#ifdef _OPENMP
-  write(*,'(a43)') 'Fortran OpenMP Stencil execution on 2D grid'
-#else
-  write(*,'(a43)') 'Fortran Serial Stencil execution on 2D grid'
-#endif
 
   if (command_argument_count().lt.2) then
     write(*,'(a17,i1)') 'argument count = ', command_argument_count()
@@ -267,138 +276,192 @@ program main
     stop 1
   endif
 
-  allocate( A(n,n), stat=err)
-  if (err .ne. 0) then
-    write(*,'(a,i3)') 'allocation of A returned ',err
-    stop 1
+  call mpi_init_thread(requested,provided)
+
+  !call ga_initialize()
+  ! ask GA to allocate enough memory for 4 matrices, just to be safe
+  max_mem = order * order * 4 * ( storage_size(one) / 8 )
+  call ga_initialize_ltd(max_mem)
+
+  me = ga_nodeid()
+  np = ga_nnodes()
+
+  !if (me.eq.0) print*,'max_mem=',max_mem
+
+#if PRK_CHECK_GA_MPI
+  ! We do use MPI anywhere, but if we did, we would need to avoid MPI collectives
+  ! on the world communicator, because it is possible for that to be larger than
+  ! the GA world process group.  In this case, we need to get the MPI communicator
+  ! associated with GA world, but those routines assume MPI communicators are integers.
+
+  call MPI_Comm_rank(world, world_rank)
+  call MPI_Comm_size(world, world_size)
+
+  if ((me.ne.world_rank).or.(np.ne.world_size)) then
+      write(*,'(a12,i8,i8)') 'rank=',me,world_rank
+      write(*,'(a12,i8,i8)') 'size=',me,world_size
+      call ga_error('MPI_COMM_WORLD is unsafe to use!!!',np)
   endif
-
-  allocate( B(n,n), stat=err )
-  if (err .ne. 0) then
-    write(*,'(a,i3)') 'allocation of B returned ',err
-    stop 1
-  endif
-
-  norm = 0.d0
-  active_points = int(n-2*r,INT64)**2
-
-#ifdef _OPENMP
-  write(*,'(a,i8)') 'Number of threads    = ',omp_get_max_threads()
 #endif
-  write(*,'(a,i8)') 'Number of iterations = ', iterations
-  write(*,'(a,i8)') 'Grid size            = ', n
-  write(*,'(a,i8)') 'Radius of stencil    = ', r
-  if (is_star) then
-    write(*,'(a,a)')  'Type of stencil      = star'
-    stencil_size = 4*r+1
-  else
-    write(*,'(a,a)')  'Type of stencil      = grid'
-    stencil_size = (2*r+1)**2
-  endif
-  write(*,'(a)') 'Data type            = double precision'
-  write(*,'(a)') 'Compact representation of stencil loop body'
-  if (tiling) then
-      write(*,'(a,i5)') 'Tile size            = ', tile_size
-  else
-      write(*,'(a)') 'Untiled'
+
+  if (me.eq.0) then
+    write(*,'(a25)') 'Parallel Research Kernels'
+    write(*,'(a50)') 'Fortran Global Arrays Stencil execution on 2D grid'
+    write(*,'(a22,i12)') 'Number of GA procs   = ', np
+    write(*,'(a,i8)') 'Number of iterations    = ', iterations
+    write(*,'(a,i8)') 'Matrix order            = ', order
   endif
 
-  call initialize_w(is_star,r,W)
+  call ga_sync()
 
-  !$omp parallel default(none)                                        &
-  !$omp&  shared(n,A,B,W,t0,t1,iterations,tiling,tile_size,is_star)   &
-  !$omp&  private(i,j,k)                                  &
-  !$omp&  reduction(+:norm)
+  ! ********************************************************************
+  ! ** Allocate space for the input and transpose matrix
+  ! ********************************************************************
 
-  ! intialize the input and output arrays
-  !$omp do
-  do j=1,n
-    do i=1,n
-      A(i,j) = cx*i+cy*j
-#if 1
-      B(i,j) = 0.d0
-#endif
+  t0 = 0.0d0
+
+  !print*,'order=',order
+  ! must cast int32 order to integer...
+  ok = ga_create(MT_DBL, int(order), int(order),'A',-1,-1, A)
+  if (.not.ok) then
+    call ga_error('allocation of A failed',100)
+  endif
+
+  ok = ga_duplicate(A,B,'B')
+  if (.not.ok) then
+    call ga_error('duplication of A as B failed ',101)
+  endif
+  call ga_zero(B)
+
+  ok = ga_duplicate(A,AT,'A^T')
+  if (.not.ok) then
+    call ga_error('duplication of A as A^T failed ',102)
+  endif
+  call ga_zero(AT)
+
+  call ga_sync()
+
+  call ga_distribution( A, me, mylo(1), myhi(1), mylo(2), myhi(2) )
+  !write(*,'(a7,5i6)') 'local:',me,mylo(1), myhi(1), mylo(2), myhi(2)
+  allocate( T(myhi(1)-mylo(1)+1,myhi(2)-mylo(2)+1), stat=err)
+  if (err .ne. 0) then
+    call ga_error('allocation of T failed',err)
+  endif
+  do j=mylo(2),myhi(2)
+    jj = j-mylo(2)+1
+    do i=mylo(1),myhi(1)
+      ii = i-mylo(1)+1
+      T(ii,jj) = real(order,REAL64) * real(j-1,REAL64) + real(i-1,REAL64)
     enddo
   enddo
-  !$omp end do
-#if 0
-  !$omp do
-  do j=r+1,n-r
-    do i=r+1,n-r
-      B(i,j) = 0.d0
-    enddo
-  enddo
-  !$omp end do
-#endif
+  !write(*,'(a8,5i6)') 'ga_put:',mylo(1), myhi(1), mylo(2), myhi(2), myhi(2)-mylo(2)+1
+  call ga_put( A, mylo(1), myhi(1), mylo(2), myhi(2), T, myhi(1)-mylo(1)+1 )
+  call ga_sync()
 
-  t0 = 0
+  ok = ma_init(MT_DBL, order*order, 0)
+  if (.not.ok) then
+    call ga_error('ma_init failed', order)
+  endif
+
+  if (order.lt.10) then
+    call ga_print(A)
+  endif
 
   do k=0,iterations
 
     ! start timer after a warmup iteration
-    !$omp barrier
-    !$omp master
     if (k.eq.1) then
-       t0 = prk_get_wtime()
+        call ga_sync()
+        t0 = ga_wtime()
     endif
-    !$omp end master
 
-    ! Apply the stencil operator
-    call apply_stencil(is_star,tiling,tile_size,r,n,W,A,B)
-
-    ! add constant to solution to force refresh of neighbor data, if any
-    !$omp do
-    do j=1,n
-      do i=1,n
-        A(i,j) = A(i,j) + 1.d0
-      enddo
-    enddo
-    !$omp end do
+    ! B += A^T
+    ! A += 1
+    call ga_transpose(A,AT)      ! C  = A^T
+    call ga_sync()               ! ga_tranpose does not synchronize after remote updates
+    call ga_add(one,B,one,AT,B)  ! B += A^T
+    call ga_add_constant(A, one) ! A += 1
+    !call ga_sync()               ! ga_add and ga_add_constant synchronize
 
   enddo ! iterations
 
-  !$omp barrier
-  !$omp master
-  t1 = prk_get_wtime()
-  !$omp end master
+  call ga_sync()
+  t1 = ga_wtime()
 
-  ! compute L1 norm in parallel
-  !$omp do
-  do j=r,n-r
-    do i=r,n-r
-      norm = norm + abs(B(i,j))
-    enddo
-  enddo
-  !$omp end do
+  trans_time = t1 - t0
 
-  !$omp end parallel
+  ! ********************************************************************
+  ! ** Analyze and output results.
+  ! ********************************************************************
 
-  stencil_time = t1 - t0
-  norm = norm / real(active_points,REAL64)
-
-  !******************************************************************************
-  !* Analyze and output results.
-  !******************************************************************************
-
-  deallocate( B )
-  deallocate( A )
-
-  ! verify correctness
-  reference_norm = real(iterations+1,REAL64) * (cx + cy);
-  if (abs(norm-reference_norm) .gt. epsilon) then
-    write(*,'(a,f13.6,a,f13.6)') 'ERROR: L1 norm = ', norm, &
-                                 ' Reference L1 norm = ', reference_norm
-  else
-    write(*,'(a)') 'Solution validates'
-#ifdef VERBOSE
-    write(*,'(a,f13.6,a,f13.6)') 'VERBOSE: L1 norm = ', norm, &
-                                 ' Reference L1 norm = ', reference_norm
-#endif
+  if (order.lt.10) then
+    call ga_print(A)
+    call ga_print(AT)
+    call ga_print(B)
   endif
 
-  flops = int(2*stencil_size+1,INT64) * active_points
-  avgtime = stencil_time/iterations
-  write(*,'(a,f13.6,a,f13.6)') 'Rate (MFlops/s): ',1.0d-6*flops/avgtime, &
-                               ' Avg time (s): ',avgtime
+  !write(*,'(a8,5i6)') 'ga_get:',mylo(1), myhi(1), mylo(2), myhi(2), myhi(2)-mylo(2)+1
+  call ga_get( B, mylo(1), myhi(1), mylo(2), myhi(2), T, myhi(1)-mylo(1)+1 )
+
+  abserr = 0.0
+  ! this will overflow if iterations>>1000
+  addit = (0.5*iterations) * (iterations+1)
+  do j=mylo(2),myhi(2)
+    jj = j-mylo(2)+1
+    do i=mylo(1),myhi(1)
+      ii = i-mylo(1)+1
+      temp = ((real(order,REAL64)*real(i-1,REAL64))+real(j-1,REAL64)) &
+           * real(iterations+1,REAL64)
+      abserr = abserr + abs(T(ii,jj) - (temp+addit))
+    enddo
+  enddo
+  call ga_dgop(MT_DBL, abserr, 1, '+')
+
+  deallocate( T )
+
+  ok = ga_destroy(AT)
+  if (.not.ok) then
+      call ga_error('ga_destroy failed',201)
+  endif
+
+  ok = ga_destroy(A)
+  if (.not.ok) then
+      call ga_error('ga_destroy failed',202)
+  endif
+
+  ok = ga_destroy(B)
+  if (.not.ok) then
+      call ga_error('ga_destroy failed',203)
+  endif
+
+  call ga_sync()
+
+  if (me.eq.0) then
+    if (abserr .lt. epsilon) then
+      write(*,'(a)') 'Solution validates'
+      avgtime = trans_time/iterations
+      bytes = 2 * int(order,INT64) * int(order,INT64) * storage_size(one)/8
+      write(*,'(a,f13.6,a,f10.6)') 'Rate (MB/s): ',(1.d-6*bytes)/avgtime, &
+             ' Avg time (s): ', avgtime
+    else
+      write(*,'(a,f30.15,a,f30.15)') 'ERROR: Aggregate squared error ',abserr, &
+             'exceeds threshold ',epsilon
+      call ga_error('Answer wrong',911)
+    endif
+  endif
+
+  call ga_sync()
+
+#ifdef PRK_GA_SUMMARY
+  if (me.eq.0) write(*,'(a)') ! add an empty line
+  call ga_summarize(.false.)
+  if (me.eq.0) then
+    call ma_print_stats()
+  endif
+#endif
+
+  call ga_terminate()
+  call mpi_finalize()
 
 end program main
+
