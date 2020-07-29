f423402ade203accdde401f850cc9a8fdbffe1f6
Jeff Hammond <jeff.science@gmail.com>
2020-07-29 13:43:46 -0700

    I don't know

--- C1z/nstream-alloc-target.c
+++ C1z/nstream-alloc-target.c
@@ -1,70 +1,9 @@
-///
-/// Copyright (c) 2019, Intel Corporation
-///
-/// Redistribution and use in source and binary forms, with or without
-/// modification, are permitted provided that the following conditions
-/// are met:
-///
-/// * Redistributions of source code must retain the above copyright
-///       notice, this list of conditions and the following disclaimer.
-/// * Redistributions in binary form must reproduce the above
-///       copyright notice, this list of conditions and the following
-///       disclaimer in the documentation and/or other materials provided
-///       with the distribution.
-/// * Neither the name of Intel Corporation nor the names of its
-///       contributors may be used to endorse or promote products
-///       derived from this software without specific prior written
-///       permission.
-///
-/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-/// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-/// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-/// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-/// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-/// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-/// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-/// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-/// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-/// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-/// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-/// POSSIBILITY OF SUCH DAMAGE.
+#pragma omp requires unified_shared_memory
 
-//////////////////////////////////////////////////////////////////////
-///
-/// NAME:    nstream
-///
-/// PURPOSE: To compute memory bandwidth when adding a vector of a given
-///          number of double precision values to the scalar multiple of
-///          another vector of the same length, and storing the result in
-///          a third vector.
-///
-/// USAGE:   The program takes as input the number
-///          of iterations to loop over the triad vectors, the length of the
-///          vectors, and the offset between vectors
-///
-///          <progname> <# iterations> <vector length> <offset>
-///
-///          The output consists of diagnostics to make sure the
-///          algorithm worked, and of timing statistics.
-///
-/// NOTES:   Bandwidth is determined as the number of words read, plus the
-///          number of words written, times the size of the words, divided
-///          by the execution time. For a vector length of N, the total
-///          number of words read and written is 4*N*sizeof(double).
-///
-///
-/// HISTORY: This code is loosely based on the Stream benchmark by John
-///          McCalpin, but does not follow all the Stream rules. Hence,
-///          reported results should not be associated with Stream in
-///          external publications
-///
-///          Converted to C++11 by Jeff Hammond, November 2017.
-///          Converted to C11 by Jeff Hammond, February 2019.
-///
-//////////////////////////////////////////////////////////////////////
-
-#include "prk_util.h"
-#include "prk_openmp.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
 
 int main(int argc, char * argv[])
 {
@@ -126,14 +65,14 @@ int main(int argc, char * argv[])
   {
     for (int iter = 0; iter<=iterations; iter++) {
 
-      if (iter==1) nstream_time = prk_wtime();
+      if (iter==1) nstream_time = omp_get_wtime();
 
       #pragma omp target teams distribute parallel for simd schedule(static) device(device)
       for (size_t i=0; i<length; i++) {
           A[i] += B[i] + scalar * C[i];
       }
     }
-    nstream_time = prk_wtime() - nstream_time;
+    nstream_time = omp_get_wtime() - nstream_time;
   }
 
   omp_target_free(C, device);
