8f6a97027e2af7f0ad2143c34c398e068c2b715a
Jeff Hammond <jeff.science@gmail.com>
2021-02-24 21:54:49 -0800

    swift
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

--- SWIFT/hello.swift
+++ SWIFT/hello.swift
@@ -1,2 +1,8 @@
 print("Hello World")
 print(CommandLine.arguments)
+print(CommandLine.arguments.count)
+
+if CommandLine.arguments.count > 2 {
+	print(CommandLine.arguments[1])
+	print(CommandLine.arguments[2])
+}
